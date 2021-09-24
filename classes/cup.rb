9d83966d6456ab277618c56e5773af57ba656ee7
Luke Hooper <laaronhooper@gmail.com>
2021-09-24 18:36:59 +1000

    classes method blocks

---
+++ classes/cup.rb
@@ -0,0 +1,12 @@
+require 'games_dice'
+
+class Cup
+    attr_accesor 
+    def initialize(dice1,dice2, dice3, dice4, dice5)
+        @dice1 = dice1
+        @dice2 = dice2   
+        @dice3 = dice3 
+        @dice4 = dice4
+        @dice5 = dice5
+    end
+end
---
+++ classes/scorecard.rb
@@ -0,0 +1,16 @@
+class Scorecard 
+    attr_accessor :name, 
+    def initialize(name)
+        @name = name
+        
+    end
+    def players(name)
+        @name = name
+    end 
+    def challenges
+    end
+    def instructions
+    end
+    def scores
+    end
+end
