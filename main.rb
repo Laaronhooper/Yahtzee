230c054dd49d794526db59fe0d71e717e98000c8
Luke Hooper <laaronhooper@gmail.com>
2021-09-25 11:51:35 +1000

    Player questions

--- main.rb
+++ main.rb
@@ -1,7 +1,26 @@
-print "Would you like to play Yahtzee? (y or yes) "
-input = gets.chomp.downcase
-if input == "yes"
-    print "Great! You'll need two players for this Tango!"
-else
-    print "Come back whe you'd like too play! "
-end     
+def play
+    print "Would you like to play again? [y/n]: "
+    case gets.chomp.downcase.strip
+    when 'y', 'yes'
+        puts 'I play' # Game.play
+    when 'n','no'
+        puts "Come back when you'd like to play!"
+    end 
+end
+
+#play
+
+def player_name
+    print "What's you're name? "
+    input_playername = gets.chomp.downcase.strip
+    puts "Your name is #{input_playername}, Correct?" 
+    case input = gets.chomp.downcase.strip
+    when 'y', 'yes'
+        puts "Ok, lets go "
+    when 'n','no'
+        puts "Ok, let's try that again."
+        self.player_name
+    end  
+end
+
+player_name
