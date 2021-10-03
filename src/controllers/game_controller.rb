e69cf1d70608f85e364db3aaa19135ffc18692b2
Luke Hooper <laaronhooper@gmail.com>
2021-10-03 13:13:08 +1100

    final product

--- src/controllers/game_controller.rb
+++
@@ -1,7 +0,0 @@
-class GameController
-    def new
-        player = Player.new
-        Views::Player.new player
-        player.save
-    end
-end
--- src/views/player/new.rb
+++
@@ -1,9 +0,0 @@
-module Views
-    module Player
-        def self.new(player)
-            print "Enter Player name: "
-            name = gets.chomp.to_s
-            player = Player.new(name)
-        end
-    end
-end
